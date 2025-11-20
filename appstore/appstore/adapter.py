305e12c6324dfb9498d67814acb648da4e0db834
Phil Owen <phillipsowen@outlook.com>
2025-11-20 14:40:25 -0500

    adding override to see what social accounts are returning for data.

--- appstore/appstore/adapter.py
+++ appstore/appstore/adapter.py
@@ -16,7 +16,6 @@ class RestrictEmailAdapter(DefaultAccountAdapter):
             )
         return email
 
-
 class LoginRedirectAdapter(DefaultAccountAdapter, DefaultSocialAccountAdapter):
     """
     For regular form login redirect the user to the correct
@@ -60,6 +59,12 @@ class LoginRedirectAdapter(DefaultAccountAdapter, DefaultSocialAccountAdapter):
         return url
     
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def populate_user(self, request, sociallogin, data):
+        user = super().populate_user(request, sociallogin, data)
+        print('sociallogin.account.extra_data:', sociallogin.account.extra_data)
+        return user
+
     def on_authentication_error(self, request, provider, error=None, exception=None, extra_context=None):
         provider_id = provider.id if provider else "unknown"
         error_code = error.name if error else "unknown"

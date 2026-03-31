be726446199670645aee2e5201a008b590cc7662
frostyfan109 <groupe@renci.org>
2026-03-31 15:46:01 -0400

    fix image logo path

--- appstore/appstore/settings/ai_sandbox_settings.py
+++ appstore/appstore/settings/ai_sandbox_settings.py
@@ -6,7 +6,7 @@ APPLICATION_BRAND = "ai_sandbox"
 PRODUCT_SETTINGS = ProductSettings(
     brand="ai_sandbox",
     title="RENCI AI Sandbox",
-    logo_url="/static/images/helx/logo.png",
+    logo_url="/static/images/ai_sandbox/logo.png",
     color_scheme=ProductColorScheme("#192d3f", "#06667d"),
     links=[],
 )
